061fd108ec1c00eb6d0164fcee974d6ebbe3c08f
angular-cli <angular-cli@angular.io>
2016-09-27 07:35:31 -0500

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { FeChallengePage } from './app.po';
+
+describe('fe-challenge App', function() {
+  let page: FeChallengePage;
+
+  beforeEach(() => {
+    page = new FeChallengePage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
