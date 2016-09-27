1ea4f69391bdda9c7598d244d12f982585bba84f
Julieta Campos <jcampos@itexico.net>
2016-09-27 16:09:31 -0500

    Initital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DataTableComponent } from './data-table/data-table.component';
+import { appRoutingProviders, routing } from './app.routes';
+
+import { CityService } from './city.service';
+import { SortByPipe } from './shared/pipes/sort-by.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent,
+    SortByPipe,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  exports: [
+    SortByPipe,
+  ],
+  providers: [appRoutingProviders, CityService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/pipes/sort-by.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sortBy'
+})
+export class SortByPipe implements PipeTransform {
+
+  transform(array: Array<any>, orderField: string, orderType: boolean): Array<string> {
+    array.sort((a: any, b: any) => {
+        if (a[orderField] == null || a[orderField].isUndefined) {
+            return orderType ? 0 - b[orderField] : b[orderField] - 0;
+        }
+        if (b[orderField] == null || b[orderField].isUndefined) {
+          return orderType ? a[orderField] - 0 : b[orderField] - 0;
+        }
+        return orderType ? a[orderField] - b[orderField] : b[orderField] - a[orderField];
+    });
+    console.log(array);
+    return array;
+  }
+
+}
